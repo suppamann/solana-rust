b88bc9c60239d4384b447ba1751a0695e3d2b081
suppamann <er.yellowflash@gmail.com>
2026-02-03 03:45:51 +0000

    day 3

---
+++ rust/src/bin/day3.rs
@@ -0,0 +1,8 @@
+fn main(){
+    let achu: String = String::from("Hello Achu");
+    println!("{}", achu);//adds new line
+
+    for i in 1..10{
+        print!("{}",i);// prints exactly as is
+    }
+}
